fbb845eae151b9bd469288513ac1ef893c16ab0d
ChloeArd <chloe.ardoise@gmail.com>
2020-12-03 14:33:21 +0100

    Exercice 70

--- exo2/debug_2.js
+++ exo2/debug_2.js
@@ -1,27 +1,26 @@
-let main = document.getElementById(main);
+let main = document.getElementById('main');
 
 main.id = "main2";
-
 main.style.display = "hidden";
 
-document.getElementById('main') = "Mon nouveau texte !";
+document.getElementById('main').innerHTML = "Mon nouveau texte !";
 
-let html = main.innerHTLM;
-
+let html = main.innerHTML = "blabla";
 alert(html);
 
 if(html) {
     console.log(html);
 }
+
 else {
-    let test = document.getElementParId('test');
+    let test = document.getElementById('test');
     if(test) {
         alert(test);
     }
 }
 
 
-let main = getElementById('main');
-main[style][display] = "block";
+let main2 = document.getElementById('main');
+main2.style.display = "block";
 
 
